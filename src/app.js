bfde7e057a2591d923fc8c715dc5ed2c57f1e5c7
leonardomenesesdev <leonardomenesesc@edu.unifor.br>
2025-04-05 21:51:17 -0300

    feat: user

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const rotasTarefas = require('./routes/tarefaRoutes');
+const rotasUsuario = require('./routes/usuarioRoutes')
 const app = express()
 app.use(express.json())
 app.use('/tarefas', rotasTarefas)
+app.use('/usuarios', rotasUsuario)
 module.exports = app;
