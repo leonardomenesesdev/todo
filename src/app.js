3bda24d958047e1219213309c39c05e71867f83e
leonardomenesesdev <leonardomenesesc@edu.unifor.br>
2025-04-12 18:51:14 -0300

    fix: cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const rotasTarefas = require('./routes/tarefaRoutes');
 const rotasUsuario = require('./routes/usuarioRoutes')
+const cors = require('cors'); 
 const app = express()
+app.use(cors()); 
 app.use(express.json())
 app.use('/tarefas', rotasTarefas)
 app.use('/usuarios', rotasUsuario)
--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 const app = require('./app.js');
+const cors = require('cors'); 
+app.use(cors()); 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
